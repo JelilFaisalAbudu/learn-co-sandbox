a5b794af39860b5c60939e46dc908af91ac67409
Jelil Abudu <abudujelilfaisal@gmail.com>
2020-10-02 06:51:22 +0000

    Done.

---
+++ conditional_statements.js
@@ -0,0 +1,10 @@
+let river = "Volta";
+
+let country = "Ghana";
+
+if (river === "Volta") {
+  console.log("This river is found in Ghana");
+}
+else {
+  console.log("I'm working on it. I'll tell you later");
+}
